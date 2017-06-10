f0806db197668f33d75ebd342b48d538e73303bc
pepliseb <eman@onet.eu>
2017-06-10 20:08:54 +0200

    test split project

---
+++ src/pl/pepliseb/Main.java
@@ -0,0 +1,13 @@
+package pl.pepliseb;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String napis = " ala ma kota ";
+        String sf1 = String.format("Name is %f", 32.3333);
+
+        System.out.println(sf1);
+
+    }
+}
