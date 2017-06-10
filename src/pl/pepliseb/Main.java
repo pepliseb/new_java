7a1d1a7dfeab51115d8126702895dca37cf2f05d
pepliseb <eman@onet.eu>
2017-06-10 20:15:48 +0200

    change

--- src/pl/pepliseb/Main.java
+++ src/pl/pepliseb/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String napis = " ala ma kota ";
-        String sf1 = String.format("Name is %f", 32.3333);
+        String sf1 = String.format("Name is %32.33f", 32.3333);
 
         System.out.println(sf1);
 
